e2a41bd177420892faa5f4ecea974b717a60410f
babypilot007 <skypirateee@gmail.com>
2023-12-12 17:55:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // Using CORS Anywhere
+        const apiUrl = 'https://aviationweather.gov/api/data/metar?ids=Kcdw&format=json';
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        setWeatherData(data);
+        console.log(data)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{weatherData ? (
+        <pre>{weatherData[0].name}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}</h1>
+      
+      {weatherData ? (
+        <pre>{weatherData[0].clouds[0].cover}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+
+
+
+
     </div>
   );
 }
