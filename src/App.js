4ec704779bce49958e13f287187530eb0b2060e8
babypilot007 <skypirateee@gmail.com>
2023-12-14 15:51:04 -0500

    added more infor

--- src/App.js
+++ src/App.js
@@ -107,19 +107,79 @@ function App() {
         <p>Loading...</p>
       )}</h2>
 
-</div>
+      </div>
 
 
       {/* KTEB */}
-<div className='TAF'> 
-      <h1>{weatherData ? (
-        <pre>{weatherData[1].icaoId}
-        </pre>
-      ) : (
-        <p>Loading...</p>
-      )}</h1>
+      <div className='TAF'>
+     
+     <h2>{weatherData ? (
+      <pre>
+          <p className ='clouds'>     
+          {
+              (() => {
+                 if(weatherData[1].clouds[0].base === null)
+                 {weatherData[1].clouds[0].base = "No Clouds"}
+                 else {weatherData[1].clouds[0].base = weatherData[1].clouds[0].base +  ' AGL'}
+              })()             
+          }  
+          </p> 
 
-</div>
+          <p className ='winds'>     
+          {
+              (() => {
+                 if(weatherData[1].wgst === null)
+                 {weatherData[1].wgst = ""}
+                 else {weatherData[1].wgst ='Gust ' + weatherData[1].wgst +  ' Kts'}
+              })()             
+          }  
+          </p> 
+
+      <h2>{weatherData ? (
+      
+      <pre>{weatherData[1].icaoId}</pre>
+             ) : (
+           <p>Loading...</p>
+         )}</h2> 
+
+                 Sky - {weatherData[1].clouds[0].cover}    
+        <br></br>clouds - {weatherData[1].clouds[0].base}        
+        <br></br>visibility - {weatherData[1].visib} sm
+        <br></br>Winds {weatherData[1].wdir}<span> </span>
+                 at {weatherData[1].wspd} kts 
+        <br></br>{weatherData[1].wgst}
+
+
+    <div className ='fltconds'>     
+          {
+              (() => {
+                  if(parseInt(weatherData[1].visib) >= 10 && parseInt(weatherData[1].clouds[0].base) > 3000 ) {
+                          return (
+                              <p className='VFR'>I'd Fly in this weather</p>
+                          )
+                      } else if ((parseInt(weatherData[1].visib) <= 5 && parseInt(weatherData[1].visib) >= 3 ) || ( parseInt(weatherData[1].clouds[0].base) <= 3000 && parseInt(weatherData[1].clouds[0].base) >= 1000)) {
+                          return (
+                          <p className='MVFR'>I probably won't Fly</p>
+                          )
+                      } else if((parseInt(weatherData[1].visib) < 3 && parseInt(weatherData[1].visib) >= 1 ) || ( parseInt(weatherData[1].clouds[0].base) < 1000 && parseInt(weatherData[1].clouds[0].base) >= 500)) {
+                          return (
+                              <p className='IFR'>Won't fly unless I need to </p>
+                          )
+                      }else if((parseInt(weatherData[1].visib) < 1) || (parseInt(weatherData[1].clouds[0].base) < 500)) {
+                        return (
+                            <p className='LIFR'>Absolutely Won't Fly </p>
+                        )
+                      }else return ( <p className='clear'>Blue skies </p>)
+              })()  
+          }  
+      </div>  
+
+      </pre>
+    ) : (
+      <p>Loading...</p>
+    )}</h2>
+
+    </div>
         
         
     
