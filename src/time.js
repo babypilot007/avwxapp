f4692e141708caf169c5c2e0ad403e665faa102c
babypilot007 <skypirateee@gmail.com>
2023-12-17 20:50:54 -0500

    added more infor

--- src/time.js
+++ src/time.js
@@ -5,14 +5,26 @@ import React from "react";
 function newtime(getTime){
 
     const date = dayjs();
-    const ctime = date.$H
+    let reptime = getTime.split(" ")
+    let reptime2 = reptime[1].split(":")
+
+    let reportTime = parseInt(reptime2[0])
+
+    if(reportTime <= 12){
+
+        reportTime = (reportTime + 12) - 5
+    }else {
+        reportTime = ((reportTime) - 12) - 5
+    }
+
+    console.log(reportTime)
 
     return(
         <div>
             {getTime}
            {
               (() => {
-                if((24 - ctime) >= 12){
+                if(parseInt(reptime2[0]) >= 12){
                   return <p>Refreshed at {date.format("hh:mm:ss")} AM </p>
                 }
                 else { return <p>Refreshed at {date.format("hh:mm:ss")} PM</p>}
