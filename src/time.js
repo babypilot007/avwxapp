cd134c750855d01375633a1d452448c14053f9b7
babypilot007 <skypirateee@gmail.com>
2023-12-19 20:03:29 -0500

    added more infor

--- src/time.js
+++ src/time.js
@@ -5,10 +5,11 @@ import React from "react";
 function newtime(getTime){
 
     const date = dayjs();
+    var isCurrent = ""
     let reptime = getTime.split(" ")
     let reptime2 = reptime[1].split(":")
 
-    let reportTime = parseInt(reptime2[0])
+    let reportTime = parseInt(reptime2[1])
 
     if(reportTime <= 12){
 
@@ -17,20 +18,28 @@ function newtime(getTime){
         reportTime = ((reportTime) - 12) - 5
     }
 
+    if(reportTime - parseInt(date.format("hh")) <= 0)
+        {
+           isCurrent = <p className="VFR">{isCurrent = "Data is Current"}</p>
+        }else isCurrent = <p className="IFR">{isCurrent = "Data is not Current"}</p>
+
+
     console.log(reportTime)
+    console.log((date.format("hh")))
+    console.log(isCurrent)
 
     return(
         <div>
             {getTime}
            {
               (() => {
-                if(parseInt(reptime2[0]) >= 12){
+                if(date.format("HH") < 12){
                   return <p>Refreshed at {date.format("hh:mm:ss")} AM </p>
                 }
                 else { return <p>Refreshed at {date.format("hh:mm:ss")} PM</p>}
              })()
             }
-    
+            <p>{isCurrent}</p>
         </div>
     )
 }
