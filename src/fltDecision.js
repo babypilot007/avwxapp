365afa716a9842b599279a726262b0b1755c7a79
babypilot007 <skypirateee@gmail.com>
2023-12-28 15:55:20 -0500

    added more infor

--- src/fltDecision.js
+++ src/fltDecision.js
@@ -4,19 +4,21 @@ import React from "react"
 function fltDecision(getSee, getCloud){
 
    
-        if(getSee === 'VFR' && getCloud === 'VFR'){
+        if(getSee === 'LIFR' || getCloud === 'LIFR'){
 
-            return <span className="VFR">- VFR</span>
-            
-        } else if(getSee === 'MVFR' && getCloud === 'MVFR'){
-            return <span className="MVFR">- MVFR</span>
-
-        }else if((getSee === 'IFR' || getCloud === 'IFR') && (getSee !== 'LIFR' || getCloud !== 'LIFR')){
-            return <span className="IFR">- IFR</span>
-        }else {
             return <span className="LIFR">- LIFR</span>
-        }
+            
+        } else if(getSee === 'IFR' || getCloud === 'IFR')
+        {
+            return <span className="IFR">- IFR</span>
 
-}
+        }else if(getSee === 'MVFR' || getCloud === 'MVFR'){
+
+            return <span className="IFR">- IFR</span>
+
+        }else return <span className="VFR">- VFR</span>
+
+
+    }
 
 export default fltDecision
