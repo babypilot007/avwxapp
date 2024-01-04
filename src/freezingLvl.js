145e54fd1a397062b5265c3e8e775e9689d650ec
babypilot007 <skypirateee@gmail.com>
2024-01-04 12:29:57 -0500

    added more infor

--- src/freezingLvl.js
+++ src/freezingLvl.js
@@ -5,8 +5,15 @@ import React from "react";
 function freezingLvl(temp){
 
     const freezingLvl = (((parseInt(temp))) / 2) * 1000;
-
+    if(freezingLvl <= 0)
     return(
+            <p className="freeze">
+           
+            <span className="title">Freezing level</span> Surface
+          
+             </p>
+        )
+   else return(
         <p className="freeze">
            
          <span className="title">Freezing level</span> - {freezingLvl} Feet
